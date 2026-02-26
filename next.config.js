b70ade4b305b38e5b7c0f51092ba2ab763ccfb8f
marketmon <etmarkwalter@gmail.com>
2026-02-26 14:12:41 -0500

    page configuration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  basePath: '/rq-ttw',
+  assetPrefix: '/rq-ttw/',
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
